b2150a4b629b53f33a2f75a220c97c4514a93209
Douglas Gabriel Ochner <douglas.ochner.ext@spms.min-saude.pt>
2023-01-25 21:23:51 +0000

    style: add stylesheets and variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  css: [
+    "@/assets/css/normalize.min.css",
+    "@/assets/css/variables.css",
+    "@/assets/css/main.css"
+  ],
 })
